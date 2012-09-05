92239494009c29ef2786394ac54a6c5a9abd0a0b
Sumod <sumod.pawgi@quest.com>
2012-09-05 11:55:14 +0530

    HelloWorld program using Git repository.

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * This is a sample program to illustrate usage of Git.
+ * @author sumod_pawgi
+ */
+package HelloWorld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World of Git!");
+
+	}
+
+}
